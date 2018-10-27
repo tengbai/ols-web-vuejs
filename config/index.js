f8dfaf43759ff0fb25c04ba00d050e4996f131b8
Hengzhe Sun <hengzhe.sun@thoughtworks.com>
2018-10-27 19:10:08 +0800

    add proxy

--- config/index.js
+++ config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'https://www.easy-mock.com/mock/5bd446ec881dab24aec3515a',
+        changeOrigin: true,
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
