a5f1ea95d49c6e5351d3b0ebab804f0831a18396
Hengzhe Sun <hengzhe.sun@thoughtworks.com>
2018-10-27 17:12:38 +0800

    feature(training) add create training route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import SignIn from '@/components/SignIn'
 import SignUp from '@/components/SignUp'
+import CreateTrainingPage from '@/components/CreateTrainingPage'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/signup',
       name: 'SignUp',
       component: SignUp
+    },
+    {
+      path: '/training/create',
+      name: 'training.create',
+      component: CreateTrainingPage
     }
   ]
 })
