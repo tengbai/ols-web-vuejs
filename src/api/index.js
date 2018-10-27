e362f80eeaf7c7b779cffbfb1d5dd705201bf0ee
Hengzhe Sun <hengzhe.sun@thoughtworks.com>
2018-10-27 18:03:20 +0800

    add create training service

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,12 @@
-import http from 'http'
+import http from './http'
 
 const API_BASE = '/api'
 
 export default {
   login () {
     return http.post(`${API_BASE}/login`)
+  },
+  createTraining (training) {
+    return http.post(`${API_BASE}/training`, training)
   }
 }
